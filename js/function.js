3e608511f06520f9d9acf3465c8e7dd48316c9de
x-WRAITH <babronkamil@gmail.com>
2022-05-18 19:47:21 +0200

    w

--- js/function.js
+++
@@ -1,8 +0,0 @@
-function showPassowrd() {
-    var x = document.getElementById("myPassword");
-    if (x.type === "password") {
-      x.type = "text";
-    } else {
-      x.type = "password";
-    }
-  }
